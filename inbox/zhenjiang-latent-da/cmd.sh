a6ca89b09dbca5eda521dbb14d8f8714d04dac75
yiqun.sun <44900555+yiqun-sun-rookie@users.noreply.github.com>
2026-08-27 14:13:16 +0800

    hpc: invoke zhenjiang deploy script with bash

--- inbox/zhenjiang-latent-da/cmd.sh
+++ inbox/zhenjiang-latent-da/cmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -eo pipefail
-exec /data1/home/sunyiq/hpc_mailbox/inbox/zhenjiang-latent-da/deploy_and_submit_smoke.sh
+exec bash /data1/home/sunyiq/hpc_mailbox/inbox/zhenjiang-latent-da/deploy_and_submit_smoke.sh
