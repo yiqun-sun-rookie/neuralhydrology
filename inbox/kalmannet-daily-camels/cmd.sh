e42f76c2dccf2bcb2421ef1c8e5bc5b63fe4e296
yiqun.sun <44900555+yiqun-sun-rookie@users.noreply.github.com>
2026-08-28 19:10:54 +0800

    hpc-mailbox: collect A38 terminal evidence with stable clock offset

--- inbox/kalmannet-daily-camels/cmd.sh
+++ inbox/kalmannet-daily-camels/cmd.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-exec bash /data1/home/sunyiq/hpc_mailbox/inbox/kalmannet-daily-camels/seq77_a38_job215801_gpu_coverage_diagnostic.sh
+exec bash /data1/home/sunyiq/hpc_mailbox/inbox/kalmannet-daily-camels/seq78_a38_a800_job215801_terminal_recollect.sh
