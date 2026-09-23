b93ebc5ce864274c7bca6685e747e72e5b6425ee
yiqun.sun <44900555+yiqun-sun-rookie@users.noreply.github.com>
2026-09-23 22:06:36 +0800

    mailbox[kalmannet-tukf09-noise-20260922]: seq=16 read-only accounting recovery

--- inbox/kalmannet-tukf09-noise-20260922/cmd.sh
+++ inbox/kalmannet-tukf09-noise-20260922/cmd.sh
@@ -4,15 +4,16 @@ set -eo pipefail
 job_id=227494
 phase=/data1/home/sunyiq/kalmannet_tukf09_scaled_noise_rehearsal_20260922/full_budget_v2
 
-echo 'SQUEUE_STATUS'
-squeue -j "$job_id" -h -o '%i|%j|%P|%T|%M|%l|%D|%C|%R'
-echo 'SACCT_STATUS'
+echo 'SACCT_HISTORY'
 sacct -X -j "$job_id" -P -n --format=JobID,JobName,Partition,AllocCPUS,ReqCPUS,AllocTRES,ReqTRES,ElapsedRaw,State,ExitCode
 echo 'EXPECTED_MARKERS'
 for relative in \
   control/deployed.json \
   basin_01047000/control/submission_attempt.json \
   basin_01047000/control/submission.json \
+  basin_01047000/control/original_tests.xml \
+  basin_01047000/control/new_gate_tests.xml \
+  basin_01047000/control/tensor_tests/tensor_tests.xml \
   basin_01047000/control/job_gate.json \
   basin_01047000/run/supervisor.json \
   basin_01047000/run/model/manifest.final.sha256.json \
