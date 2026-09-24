4d1534e81ddff940b2b2bfcc3a7bb7329ad65a34
yiqun.sun <44900555+yiqun-sun-rookie@users.noreply.github.com>
2026-09-25 06:36:54 +0800

    mailbox[kalmannet-hamid-weights-20260923]: sequence 30

--- inbox/kalmannet-hamid-weights-20260923/cmd.sh
+++ inbox/kalmannet-hamid-weights-20260923/cmd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eo pipefail
-sequence=29
+sequence=30
 root=/data1/home/sunyiq/kalmannet_hamid_weights_20260923_v1
 date -u '+SNAPSHOT_UTC=%Y-%m-%dT%H:%M:%SZ'
 printf '\nARRAY_QUEUE\n'
